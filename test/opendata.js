4a42e29062c0da22e068b7356d37ad405881b1f3
kitty1010ki <kitty1010ki@gmail.com>
2018-06-26 19:48:12 +0800

    f123

--- test/opendata.js
+++ test/opendata.js
@@ -33,7 +33,7 @@
             return i;
             }
             if(document.getElementById("location-list")){
-            var locations=['基隆市','新竹市','臺北市','桃園市','新竹縣','苗栗縣','臺中市','彰化縣','雲林縣','南投縣','嘉義縣','嘉義市','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','新北市','連江縣','金門縣','澎湖縣'];
+            var locations=['基隆市','臺中市','雲林縣'];
             var LocationSelect=document.getElementById("location-list");
             var inner="";
             for(var i=0;i<locations.length;i++){
@@ -43,49 +43,11 @@
             var sectors=new Array();
             //基隆市
             sectors[0]=['基隆'];
-            //新竹市
-            sectors[1]=['新竹'];
-            //臺北市
-            sectors[2]=['士林','中山','萬華','古亭','松山','大同','陽明'];
-            //桃園市
-            sectors[3]=['桃園','大園','觀音','平鎮','龍潭','中壢'];
-            //新竹縣
-            sectors[4]=['湖口','竹東'];
-            //苗栗縣
-            sectors[5]=['頭份','苗栗','三義'];
-            //臺中市
-            sectors[6]=['豐原','沙鹿','大里','忠明','西屯'];
-            //彰化縣
-            sectors[7]=['彰化','線西','二林'];
-            //雲林縣
-            sectors[8]=['斗六','崙背','臺西','麥寮'];
-            //南投縣
-            sectors[9]=['南投','竹山','埔里'];
-            //嘉義縣
-            sectors[10]=['新港','朴子'];
-            //嘉義市
-            sectors[11]=['嘉義'];
-            //臺南市
-            sectors[12]=['新營','善化','安南','臺南'];
-            //高雄市
-            sectors[13]=['美濃','橋頭','仁武','鳳山','大寮','林園','楠梓','左營','前金','前鎮','小港','復興'];
-            //屏東縣
-            sectors[14]=['屏東','潮州','恆春'];
-            //臺東縣
-            sectors[15]=['臺東','關山'];
-            //花蓮縣
-            sectors[16]=['花蓮'];
-            //宜蘭縣
-            sectors[17]=['宜蘭','東山'];
-            //新北市
-            sectors[18]=['汐止','萬里','新店','土城','板橋','新莊','菜寮','林口','淡水','三重','永和','富貴角'];
-            //連江縣
-            sectors[19]=['馬祖'];
-            //金門縣
-            sectors[20]=['金門'];
-            //澎湖縣
-            sectors[21]=['馬公'];
-            function changeLocation(index){
+            // //臺中市
+            sectors[1]=['豐原','沙鹿','大里','忠明','西屯'];
+            // //雲林縣
+            sectors[2]=['斗六','崙背','臺西','麥寮'];
+              function changeLocation(index){
               var Sinner="";
               for(var i=0;i<sectors[index].length;i++){
                 Sinner=Sinner+'<option value=i>'+sectors[index][i]+'</option>';
@@ -109,7 +71,7 @@
 
 
             }
-
+            
             function call(){
                 $.ajax({
                     type: "GET",
@@ -139,7 +101,12 @@
                     document.getElementById('NO').innerHTML=NO;
                     document.getElementById('STU').innerHTML=Status;
 
-              setup();        
+                    setup(); 
+                    function setup(){
+                      if(cpf){
+                          cpf.setPinMode('["resetPin"],["grove_rgblcd_begin", 16, 2],["setPinMode", "digital", 5, "TONE"],["setPinMode", "digital", 6, "OUTPUT"]');
+                      }
+                  }      
               switch (Status){
                 case "良好":
                   cpf.SetSpeech("On","cmn-Hant-TW",SiteName+"狀態良好",0.1,2);
@@ -207,8 +174,4 @@
             return e.getElementsByTagName(key)[i].firstChild != null ? e.getElementsByTagName(key)[i].firstChild.nodeValue : '無資料';
           }
            
-          function setup(){
-              if(cpf){
-                  cpf.setPinMode('["resetPin"],["grove_rgblcd_begin", 16, 2],["setPinMode", "digital", 5, "TONE"],["setPinMode", "digital", 6, "OUTPUT"]');
-              }
-          }
+          
